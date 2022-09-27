5a71aca196c5ab1b02e91757f61972f9fdf0645b
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2022-09-27 18:35:33 +0100

    Mock data

--- components/MockData/MockData.tsx
+++ components/MockData/MockData.tsx
@@ -3,24 +3,14 @@ import { FC } from 'react'
 import { Props } from './types'
 
 const MockData: FC<Props> = ({ data }) => {
-  console.log({ data })
-
-  //   const jsonData = Object.assign(
-  //     {},
-  //     data.map(({ field_name, field_type }) => ({
-  //       field_name: renderField(field_type)
-  //     }))
-  //   )
-
   const jsonData = data.reduce(
-    (a, { field_name, field_type }) => ({ ...a, [field_name]: field_type }),
+    (a, { field_name, field_type }) => ({ ...a, [field_name]: renderField(field_type) }),
     {}
   )
 
   return (
     <div>
       <h1>Mock Data</h1>
-
       <code>{JSON.stringify(jsonData)}</code>
     </div>
   )
@@ -34,6 +24,8 @@ const renderField = (type: string): string => {
       return faker.internet.email()
     case 'first name':
       return faker.name.firstName()
+    case 'id':
+      return faker.datatype.uuid()
     default:
       return ''
   }
