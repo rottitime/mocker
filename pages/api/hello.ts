539b58d1fa4ab9065f0b564c97ee4523295b7f6b
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2022-09-27 21:59:32 +0100

    API test update

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,10 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiResponse } from 'next'
+import { NextRequest } from 'next/server'
 
 type Data = {
   name: string
 }
 
-export default function handler(res: NextApiResponse<Data>) {
+export default function handler(_req: NextRequest, res: NextApiResponse<Data>) {
   res.status(200).json({ name: 'John Doe' })
 }
