24b6ff4f07f67f042af8db8b1f414990942eef68
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2022-09-28 22:40:28 +0100

    cors setup

--- pages/api/mock.ts
+++ pages/api/mock.ts
@@ -1,9 +1,14 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { decodeObject } from '@/lib'
+import { decodeObject, runMiddleware } from '@/lib'
 import { Fields } from '@/components/MockRequirementsForm/types'
 import { faker } from '@faker-js/faker'
+import Cors from 'cors'
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+const cors = Cors({
+  methods: ['GET']
+})
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { fields } = req.query
 
   if (fields) {
@@ -15,6 +20,8 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
       }),
       {}
     )
+    await runMiddleware(req, res, cors)
+
     res.status(200).json(jsonData)
   }
 }
