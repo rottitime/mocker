c1e92a21600b455cc47421333917ef010c7bbfb4
Jas Aggarwal <webmaster@jaspaul.com>
2022-09-28 22:42:51 +0100

    cors setup (#10)

--- lib/url-utils.ts
+++ lib/url-utils.ts
@@ -1,4 +1,25 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
 export const encodeObject = (data: object): string =>
   encodeURIComponent(JSON.stringify(data))
 
 export const decodeObject = (data: string) => JSON.parse(decodeURIComponent(data))
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+export const runMiddleware = (
+  req: NextApiRequest,
+  res: NextApiResponse,
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  fn: Function
+) => {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: unknown) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
+}
