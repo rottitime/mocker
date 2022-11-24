9153154aef806c9c22b4ef220a5759b4835802d2
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2022-11-24 16:01:02 +0000

    Tidy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,6 @@
 import { MockRequirementsForm } from '@/components'
 import { encodeObject } from '@/lib'
-// import styled from '@emotion/styled'
 import Router from 'next/router'
-import tw, { styled, css } from 'twin.macro'
-// import { css } from '@emotion/core'
-
-const TwinBlock = styled.div`
-  border: 10px solid green;
-  ${tw`border border-b-cyan-900 rounded`}
-`
-
-const StyledBlock = styled.div`
-  border: 3px solid red;
-`
 
 const Home = () => {
   return (
@@ -22,8 +10,6 @@ const Home = () => {
       <div style={{ display: 'flex', gap: 30 }}>
         <div>
           <h2>Form</h2>
-          <TwinBlock>Twin</TwinBlock>
-          <StyledBlock>Loerem</StyledBlock>
           <MockRequirementsForm
             onSubmit={({ fields }) => {
               Router.push({
