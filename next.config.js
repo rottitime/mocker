551ee3234787eaca9e0e6edf46ebbac20080963b
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2022-11-25 12:50:30 +0000

    layout with title

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  trailingSlash: true,
   reactStrictMode: true,
   experimental: { appDir: false },
   swcMinify: true,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  publicRuntimeConfig: {
+    siteTitle: 'Mocker'
+  }
 }
 
 module.exports = nextConfig
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,9 +1,13 @@
 import { ReactNode } from 'react'
+import getConfig from 'next/config'
+
+const { publicRuntimeConfig } = getConfig()
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <>
+    <div>
+      <nav>{publicRuntimeConfig.siteTitle}</nav>
       <main>{children}</main>
-    </>
+    </div>
   )
 }
