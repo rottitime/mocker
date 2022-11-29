e34e09876ef2632ca5e61dd64eccdbbce169cd88
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2022-11-29 19:10:54 +0000

    background dots hover state

--- src/components/BackgroundDots.tsx
+++ src/components/BackgroundDots.tsx
@@ -1,5 +1,13 @@
-export default function BackgroundDots() {
+type Props = {
+  hover?: boolean
+}
+
+export default function BackgroundDots({ hover }: Props) {
   return (
-    <div className="absolute left-0 top-0 z-10 h-screen w-screen bg-gradient-radial from-white/10 to-transparent bg-[length:12vmin_12vmin] bg-left-top transition-all duration-700" />
+    <div
+      className={`absolute left-0 top-0 z-10 h-screen w-screen bg-gradient-radial from-white/10 to-transparent bg-[length:12vmin_12vmin] bg-left-top transition-all duration-700 ${
+        hover ? 'bg-[length:11vmin_11vmin] opacity-50' : ''
+      }`}
+    />
   )
 }
