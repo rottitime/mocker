1fe4749265a72f69e2b6d8a1dea2ec0d59c2db1f
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2022-11-29 16:05:19 +0000

    Select component

---
+++ src/components/Select.tsx
@@ -0,0 +1,34 @@
+import { forwardRef, SelectHTMLAttributes } from 'react'
+
+type Options = {
+  value?: string
+  name: string
+}
+
+type Props = {
+  options: Options[]
+} & SelectHTMLAttributes<HTMLSelectElement>
+
+const Select = forwardRef<HTMLSelectElement, Props>(({ options, ...props }, ref) => {
+  const filteredOptions: Options[] = options.map((name) =>
+    typeof name === 'string' ? { value: name, name } : name
+  )
+
+  return (
+    <select
+      {...props}
+      ref={ref}
+      className="mb-6 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+    >
+      {filteredOptions.map(({ value, name }, i) => (
+        <option value={value} key={value}>
+          {name}
+        </option>
+      ))}
+    </select>
+  )
+})
+
+Select.displayName = 'Select'
+
+export default Select
