6854c8b680a56b2246e1b5656de66171239909e9
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2022-11-29 12:07:41 +0000

    background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,21 +6,14 @@ import Router from 'next/router'
 const Home = () => {
   return (
     <>
-      <h1>Mocker v1</h1>
-
-      <div style={{ display: 'flex', gap: 30 }}>
-        <div>
-          <h2>Form</h2>
-          <MockRequirementsForm
-            onSubmit={({ fields }) => {
-              Router.push({
-                pathname: '/preview',
-                query: { fields: encodeObject(fields) }
-              })
-            }}
-          />
-        </div>
-      </div>
+      <MockRequirementsForm
+        onSubmit={({ fields }) => {
+          Router.push({
+            pathname: '/preview',
+            query: { fields: encodeObject(fields) }
+          })
+        }}
+      />
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,15 +14,7 @@ module.exports = {
     },
     extend: {
       colors: {
-        blue: '#1fb6ff',
-        purple: '#7e5bef',
-        pink: '#ff49db',
-        orange: '#ff7849',
-        green: '#13ce66',
-        yellow: '#ffc82c',
-        'gray-dark': '#273444',
-        gray: '#8492a6',
-        'gray-light': '#d3dce6'
+        stone: '#1f1f26'
       },
       spacing: {
         128: '32rem',
