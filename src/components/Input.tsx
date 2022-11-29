eb650774b1cbe7e6aad882596af74b40f12bab9f
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2022-11-29 15:40:03 +0000

    keuup effect

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,21 +1,32 @@
-import { forwardRef, InputHTMLAttributes } from 'react'
+import { forwardRef, InputHTMLAttributes, useState } from 'react'
 
 type Props = {
   label: string
   error?: string
 } & InputHTMLAttributes<HTMLInputElement>
 
-const Input = forwardRef<HTMLInputElement, Props>(({ label, error, ...props }, ref) => (
-  <>
-    <label>{label}</label>
-    <input
-      ref={ref}
-      className="rounded border-2 border-white/50 bg-white/5 px-3.5 py-2 text-3xl text-white/50 outline-none transition-all duration-1000 focus:border-white/75 focus:text-white/75	"
-      {...props}
-    />
-    {!!error && <p>{error}</p>}
-  </>
-))
+const Input = forwardRef<HTMLInputElement, Props>(({ label, error, ...props }, ref) => {
+  const [keyup, setKeyup] = useState(false)
+  return (
+    <>
+      <label>{label}</label>
+      <input
+        ref={ref}
+        className={`rounded border-2 border-white/50 bg-white/5 px-3.5 py-2 text-3xl text-white/50 outline-none transition-all duration-100 focus:border-white/75 focus:text-white/75 ${
+          keyup ? 'keyup' : ''
+        }	`}
+        onKeyPress={(e) => {
+          setKeyup(true)
+          setTimeout(function () {
+            setKeyup(false)
+          }, 100)
+        }}
+        {...props}
+      />
+      {!!error && <p>{error}</p>}
+    </>
+  )
+})
 
 Input.displayName = 'Input'
 
