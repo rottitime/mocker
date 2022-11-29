253b03b960b77d866eec80e6713eee1c5d1d6fc0
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2022-11-29 17:30:52 +0000

    Button styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import './global.css'
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className="text-sky-300">
+      <body>
         <header className="bg-indigo-700 py-2 px-4">
           <div className="container mx-auto px-4">
             <a href="/">
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,7 +5,7 @@ type Props = ButtonHTMLAttributes<HTMLButtonElement>
 const Button = (props: Props) => {
   return (
     <button
-      className="rounded bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-700"
+      className="inline-flex items-center gap-3 rounded bg-sky-300 py-2 px-4 font-bold uppercase text-white transition-all hover:bg-sky-800"
       {...props}
     />
   )
