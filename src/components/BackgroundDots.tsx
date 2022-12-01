151dc8897ea7f029d5d849943ee82a63e9f61207
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2022-12-01 15:56:32 +0000

    delay to prevent jumping

--- src/components/BackgroundDots.tsx
+++ src/components/BackgroundDots.tsx
@@ -8,7 +8,7 @@ export default function BackgroundDots() {
   return (
     <div
       style={{ backgroundPositionY: (focusField || 0) * 100 + 'px' }}
-      className={`absolute left-0 top-0 z-10 h-screen w-screen bg-gradient-radial from-white/10 to-transparent bg-[length:12vmin_12vmin] bg-left-top transition-all duration-700 ${
+      className={`absolute left-0 top-0 z-10 h-screen w-screen bg-gradient-radial from-white/10 to-transparent bg-[length:12vmin_12vmin] bg-left-top transition-all delay-75 duration-700 ${
         animate
           ? `bg-[length:11vmin_11vmin] 
         opacity-50 `
