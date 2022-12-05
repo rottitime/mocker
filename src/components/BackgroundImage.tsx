e50571558b2842de79f8a11d5c170ac9cdca2c39
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2022-12-05 16:24:40 +0000

    working version of multiple bg

--- src/components/BackgroundImage.tsx
+++ src/components/BackgroundImage.tsx
@@ -2,36 +2,18 @@ import { useUiContext } from '@/context/UiContext'
 import { percentageRange } from '@/lib'
 
 export default function BackgroundImage() {
-  const { focusField } = useUiContext()
+  const { focusField, totalFields } = useUiContext()
   const animate = typeof focusField !== 'undefined'
+  const percentage = animate
+    ? percentageRange((focusField / totalFields) * 100, 45, 60)
+    : 40
 
-  const hoverStyles = animate
-    ? `bg-[length:100vmax] opacity-10 `
-    : `bg-[length:110vmax] bg-[position:center_${40}%]`
+  const hoverStyles = animate ? `bg-[length:100vmax] opacity-10 ` : `bg-[length:110vmax] `
 
   return (
     <div
-      className={`bg- absolute left-0 top-0 z-0  h-screen w-screen   bg-opacity-20 bg-[url('/img/background.jpg')] transition-all delay-100 duration-700 ${hoverStyles}`}
-    >
-      focusField: {focusField}
-      <br />
-      animate: {animate}
-    </div>
+      style={{ backgroundPositionY: percentage + '%' }}
+      className={`bg- absolute left-0 top-0 z-0  h-screen w-screen   bg-opacity-20 bg-[url('/img/background.jpg')] bg-[position:center_40%] transition-all delay-100 duration-700 ${hoverStyles}`}
+    ></div>
   )
 }
-
-// #menu[data-active-index="0"] > #menu-background-image {
-//   background-position: center 45%;
-// }
-
-// #menu[data-active-index="1"] > #menu-background-image {
-//   background-position: center 50%;
-// }
-
-// #menu[data-active-index="2"] > #menu-background-image {
-//   background-position: center 55%;
-// }
-
-// #menu[data-active-index="3"] > #menu-background-image {
-//   background-position: center 60%;
-// }
