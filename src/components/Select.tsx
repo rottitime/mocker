a9c9cc5c24dc43701f0995ac4e8b2e79279ebe0f
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2023-01-13 11:59:51 +0000

    select default value warning fix

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -22,11 +22,12 @@ const Select = forwardRef<HTMLSelectElement, Props>(
       <div>
         <select
           {...props}
+          defaultValue=""
           ref={ref}
           className="w-full rounded bg-slate-100 px-3.5 py-2 text-black outline-none transition-all duration-100"
         >
           {placeholder && (
-            <option value="" disabled selected>
+            <option value="" disabled>
               {placeholder}
             </option>
           )}
