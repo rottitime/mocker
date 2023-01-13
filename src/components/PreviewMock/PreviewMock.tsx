f1e9c98bc955e917c24550281373143ceb313fee
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2023-01-13 11:27:44 +0000

    Copy feature

--- src/components/PreviewMock/PreviewMock.tsx
+++ src/components/PreviewMock/PreviewMock.tsx
@@ -3,11 +3,20 @@ import { Props } from './types'
 import { fieldsToMockJson } from '@/lib/data-test'
 import SyntaxHighlighter from 'react-syntax-highlighter'
 import { dracula } from 'react-syntax-highlighter/dist/esm/styles/hljs'
+import CopyToClipboard from 'react-copy-to-clipboard'
 
-const PreviewMock: FC<Props> = ({ data }) => (
-  <SyntaxHighlighter language="json" style={dracula} wrapLines>
-    {JSON.stringify(fieldsToMockJson(data), null, 2)}
-  </SyntaxHighlighter>
-)
+const PreviewMock: FC<Props> = ({ data }) => {
+  const text = JSON.stringify(fieldsToMockJson(data), null, 2)
+  return (
+    <>
+      <SyntaxHighlighter language="json" style={dracula} wrapLines>
+        {text}
+      </SyntaxHighlighter>
+      <CopyToClipboard text={text} onCopy={() => alert('Copied')}>
+        <button>Copy</button>
+      </CopyToClipboard>
+    </>
+  )
+}
 
 export default PreviewMock
