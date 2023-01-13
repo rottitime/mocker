6b7183d37c0ce120ecb492b5e8c37d4a814a7f55
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2023-01-13 12:45:46 +0000

    Window warning fix

--- src/components/PreviewUrl/PreviewUrl.tsx
+++ src/components/PreviewUrl/PreviewUrl.tsx
@@ -2,7 +2,10 @@ import { Props } from './types'
 import { encodeObject } from '@/lib'
 
 const PreviewUrl = ({ fields }: Props) => {
-  const url = `${window.location.origin}/api/mock?fields=${encodeObject(fields)}`
+  const origin =
+    typeof window !== 'undefined' && window.location.origin ? window.location.origin : ''
+
+  const url = `${origin}/api/mock?fields=${encodeObject(fields)}`
 
   return (
     <div className="mb-4  overflow-x-scroll whitespace-nowrap rounded bg-gray-200 py-3">
