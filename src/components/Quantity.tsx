0a523682ff07cb60b81a722c6673665daa1be466
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2023-02-03 17:53:15 +0000

    uni test fix

--- src/components/Quantity.tsx
+++ src/components/Quantity.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { useId, useRef, useState } from 'react'
+import Input from './Input'
 
 type Props = {
   defaultValue?: number
@@ -9,6 +10,7 @@ type Props = {
 function Quantity({ onChange, defaultValue }: Props) {
   const id = useId()
   const checkboxId = `single_${id}`
+  const inputId = `input_${id}`
   const inputRef = useRef<HTMLInputElement>(null)
   const valueRef = useRef<number>()
   const [checked, setChecked] = useState(false)
@@ -27,8 +29,26 @@ function Quantity({ onChange, defaultValue }: Props) {
         }}
       />
 
-      <input
+      <label htmlFor={inputId}>Rows</label>
+
+      <Input
+        id={inputId}
+        data-testid="input-quantity"
+        aria-label="Name"
+        placeholder="e.g. 10"
+        defaultValue={defaultValue}
+        disabled={checked}
+        ref={inputRef}
+        onChange={(e) => {
+          valueRef.current = parseInt(e.currentTarget.value)
+          !checked && onChange(valueRef.current)
+        }}
+        // error={errors.fields?.[index]?.field_name && 'This is a required field'}
+      />
+
+      {/* <input
         type="number"
+        id={inputId}
         ref={inputRef}
         defaultValue={defaultValue}
         disabled={checked}
@@ -37,7 +57,7 @@ function Quantity({ onChange, defaultValue }: Props) {
           valueRef.current = parseInt(e.currentTarget.value)
           !checked && onChange(valueRef.current)
         }}
-      />
+      /> */}
     </>
   )
 }
