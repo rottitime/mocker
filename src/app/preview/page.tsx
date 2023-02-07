98622eb03bf17a7cfbd2efa36f03264c9edb08fe
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2023-02-07 14:42:14 +0000

    Copy button

--- src/app/preview/page.tsx
+++ src/app/preview/page.tsx
@@ -1,8 +1,22 @@
-import { MockRequirementsForm, Card } from '@/components'
+import { MockRequirementsForm, Card, Loading } from '@/components'
 import dynamic from 'next/dynamic'
 
-const PreviewUrl = dynamic(() => import('@/components/PreviewUrl'), { ssr: false })
-const PreviewMock = dynamic(() => import('@/components/PreviewMock'), { ssr: false })
+const PreviewUrl = dynamic(() => import('@/components/PreviewUrl'), {
+  ssr: false,
+  loading: () => (
+    <div className="text-center">
+      <Loading />
+    </div>
+  )
+})
+const PreviewMock = dynamic(() => import('@/components/PreviewMock'), {
+  ssr: false,
+  loading: () => (
+    <div className="text-center">
+      <Loading />
+    </div>
+  )
+})
 
 const PreviewPage = () => (
   <div className="flex gap-10 [&>div]:w-1/2 [&>div]:grow">
@@ -12,6 +26,9 @@ const PreviewPage = () => (
     <Card>
       <h2>Your API </h2>
       <PreviewUrl />
+
+      <hr className="mx-4 my-4" />
+
       <h2>Sample data</h2>
       <PreviewMock />
     </Card>
--- src/components/PreviewUrl.tsx
+++ src/components/PreviewUrl.tsx
@@ -9,13 +9,12 @@ const PreviewUrl = () => {
   const url = origin && params && `${origin}/api/mock?${params}`
 
   return (
-    <div className="relative  mb-4  break-words rounded bg-gray-200 py-3 px-2">
+    <div className="relative  break-words rounded bg-gray-200 py-3 px-2">
       {!!url && (
         <a href={url} target="_blank" rel="noreferrer">
           {url}
         </a>
       )}
-
       <div className="absolute top-2 right-2">
         <CopyButton text={url} />
       </div>
