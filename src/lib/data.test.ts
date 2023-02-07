e0dd115891fea15f520a5cddb13670ca708f0ec2
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2023-02-07 12:07:52 +0000

    better unit test against object

--- src/lib/data.test.ts
+++ src/lib/data.test.ts
@@ -1,11 +1,12 @@
 import { fieldsToMockJson } from './data'
 
 describe('fieldsToMockJson()', () => {
-  it('Return types', () => {
-    expect(
-      fieldsToMockJson([{ field_name: 'test1', field_type: 'email' }], 0)
-    ).toBeInstanceOf(Object)
-
+  it('As object', () => {
+    const data = fieldsToMockJson([{ field_name: 'test1', field_type: 'email' }], 0)
+    expect(data).toBeInstanceOf(Object)
+    expect(data).toHaveProperty('test1')
+  })
+  it('As Array', () => {
     expect(
       fieldsToMockJson([{ field_name: 'test1', field_type: 'email' }], 1)
     ).toHaveLength(1)
