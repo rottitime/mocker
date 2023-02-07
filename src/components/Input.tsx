23ae3cccd445ab76dea95debc306808570b25692
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2023-02-07 11:56:09 +0000

    disabled opacity

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -50,7 +50,7 @@ const Input = forwardRef<HTMLInputElement, Props>(
             ref={ref}
             id={inputId}
             autoFocus
-            className={`${className} w-full  rounded  bg-slate-100 px-3.5 py-2 text-black    transition-all duration-100 disabled:border-slate-200 disabled:bg-slate-50 disabled:text-slate-500 disabled:shadow-none  ${
+            className={`${className} w-full  rounded  bg-slate-100 px-3.5 py-2 text-black    transition-all duration-100 disabled:border-slate-200 disabled:bg-slate-50 disabled:text-slate-500 disabled:opacity-60 disabled:shadow-none  ${
               keyup ? 'keyup' : ''
             } ${
               !!error || errorStylingOnly
