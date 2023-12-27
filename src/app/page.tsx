eb2b51f10f88b220035d924e24350e0598bf8f8e
Jas Aggarwal <webmaster@jaspaul.com>
2023-12-27 17:43:35 +0000

    content (#54)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,33 @@ import { MockRequirementsForm } from '@/components'
 const Home = () => (
   <>
     <Card>
-      <h2 className="mb-8 text-xl font-extrabold">
-        Faking it till you make it, with our fake API app
-      </h2>
+      <div className="mb-8">
+        <h2 className="mb-3 text-xl font-extrabold">
+          Faking it till you make it, with our fake API app
+        </h2>
+
+        <p className="mb-3 text-sm">
+          Thank you for visiting. Due to personal commitments and a demanding full-time
+          job, I can no longer dedicate time to improve APIMock.net. Your support has been
+          incredible, and I'm reaching out to the community for assistance in keeping the
+          site alive.
+        </p>
+        <p className="text-sm">
+          If you're a developer, your contributions in code or bug fixes are immensely
+          valuable. Visit{' '}
+          <a
+            href="https://github.com/rottitime/mocker"
+            className="text-blue-700 decoration-wavy"
+          >
+            github.com/rottitime/mocker
+          </a>{' '}
+          and add requests, suggestions and code on how to get involved. Alternatively, if
+          coding isn't your expertise, help us by spreading the word to those who might
+          contribute. I'm grateful for your understanding and support. Let's work together
+          to ensure that apimock.net continues to thrive and serve the community. Thank
+          you for being part of this journey.
+        </p>
+      </div>
 
       <MockRequirementsForm
         defaultValues={{
@@ -16,8 +40,7 @@ const Home = () => (
             { field_name: 'first_name', field_type: 'first_name' },
             { field_name: 'email', field_type: 'email' },
             { field_name: 'profile_picture', field_type: 'image' },
-            { field_name: 'is_admin', field_type: 'boolean' },
-
+            { field_name: 'is_admin', field_type: 'boolean' }
           ],
           rows: 11
         }}
